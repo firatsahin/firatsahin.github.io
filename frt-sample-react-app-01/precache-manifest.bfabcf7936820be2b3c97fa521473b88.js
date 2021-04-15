f7ccd6aadce768d9fc584610e9b8ca32eb309098
Guney Firat Sahin <guneyfiratsahin@gmail.com>
2021-04-15 03:02:16 +0300

    frt-sample-react-app build deployed

---
+++ frt-sample-react-app-01/precache-manifest.bfabcf7936820be2b3c97fa521473b88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07aaf3590f1c4adbd5e5c8fa3eb492bb",
+    "url": "/frt-sample-react-app-01/index.html"
+  },
+  {
+    "revision": "9f84f06e3207397c9e62",
+    "url": "/frt-sample-react-app-01/static/css/main.84c06acc.chunk.css"
+  },
+  {
+    "revision": "0cb0d94e4e5887525b88",
+    "url": "/frt-sample-react-app-01/static/js/2.95d10c59.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/frt-sample-react-app-01/static/js/2.95d10c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e574d1d4a7608bda3499",
+    "url": "/frt-sample-react-app-01/static/js/3.a627c452.chunk.js"
+  },
+  {
+    "revision": "3003b51ce73d7faa0593",
+    "url": "/frt-sample-react-app-01/static/js/4.c451084c.chunk.js"
+  },
+  {
+    "revision": "c15485707ee979e6bbaf",
+    "url": "/frt-sample-react-app-01/static/js/5.adb5527b.chunk.js"
+  },
+  {
+    "revision": "cd584fe5388f7e95e777",
+    "url": "/frt-sample-react-app-01/static/js/6.5cf6395b.chunk.js"
+  },
+  {
+    "revision": "7080be82c21a96f4b742",
+    "url": "/frt-sample-react-app-01/static/js/7.afd0ba58.chunk.js"
+  },
+  {
+    "revision": "4f88ca2469b29c7ccf83",
+    "url": "/frt-sample-react-app-01/static/js/8.538f2cee.chunk.js"
+  },
+  {
+    "revision": "9f84f06e3207397c9e62",
+    "url": "/frt-sample-react-app-01/static/js/main.e90db0aa.chunk.js"
+  },
+  {
+    "revision": "208c19d4e658f0ef3aa0",
+    "url": "/frt-sample-react-app-01/static/js/runtime-main.3bf78823.js"
+  }
+]);
